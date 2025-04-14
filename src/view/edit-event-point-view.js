de497f6848be9279b8c5025d74d77f85557d9ec4
David <gidlos55@gmail.com>
2025-04-14 18:11:32 +0300

    Исправит проблему с лишними пространством между датами

--- src/view/edit-event-point-view.js
+++ src/view/edit-event-point-view.js
@@ -134,10 +134,9 @@ function createEditPointTemplate({type, name, startTime, endTime, price , allOff
 
                   <div class="event__field-group  event__field-group--time">
                     <label class="visually-hidden" for="event-start-time-1">From</label>
-                    <input class="event__input  event__input--time-1" id="event-start-time-1" type="text" name="event-start-time" value="${eventStartDate}">
-                    &mdash;
+                    <input class="event__input event__input--time event__input--time-1" id="event-start-time-1" type="text" name="event-start-time" value="${eventStartDate}">&mdash;
                     <label class="visually-hidden" for="event-end-time-1">To</label>
-                    <input class="event__input  event__input--time-2" id="event-end-time-1" type="text" name="event-end-time" value="${eventEndDate}">
+                    <input class="event__input  event__input--time event__input--time-2" id="event-end-time-1" type="text" name="event-end-time" value="${eventEndDate}">
                   </div>
 
                   <div class="event__field-group  event__field-group--price">
